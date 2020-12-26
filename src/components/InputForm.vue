e98e37f083a7d5d1b7af0e7632e73b284e8765bb
geofreybundala <bundalageofrey2@gmail.com>
2020-12-26 12:21:22 +0300

    feat:form for adding todo

---
+++ src/components/InputForm.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="mt-4 mx-3"> 
+        <label class="block">
+            <!-- <span class="text-gray-700">Title</span> -->
+            <input type="text" class="mt-1 block w-full" placeholder="Please white your Todo Title">
+        </label>
+        <label class="block">
+              <span class="text-gray-700">Description</span>
+              <textarea class="mt-1 block w-full h-24" rows="3" placeholder="Enter some long form content."></textarea>
+        </label>
+
+            <label class="block">
+            <span class="text-gray-700">Time</span>
+            <input type="datetime-local" class="mt-1 block w-full" placeholder="john@example.com">
+        </label>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'InputForm'
+}
+</script>
--- src/components/Todo-Home.vue
+++ src/components/Todo-Home.vue
@@ -6,16 +6,23 @@
             </div>
             <List/>
         </div>
-        <div class="bg-gray-100 shadow-md mx-3 h-full">add items here</div>
+        <div class="bg-gray-100 shadow-md mx-3 h-full">
+            <div>
+                <h4 class="pt-3 mb-0 text-xl text-blue-500">ADD TODO's</h4>
+            </div>
+            <InputForm/>
+        </div>
     </div>
 </template>
 
 <script>
 import List from './List.vue';
+import InputForm from './InputForm.vue';
 export default {
     name:'Todo-Home',
     components:{
-        List
+        List,
+        InputForm
     }
 
 }
